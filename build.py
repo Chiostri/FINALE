0b00466e7cbbc810fbbd3725e0faddd00f479650
Erika Chiostri <lechiostri@comcast.net>
2021-02-10 14:58:18 -0800

    add

--- build.py
+++ build.py
@@ -3,31 +3,31 @@
 #assembles new file by combining
 #writes the new file to a brand new file
 
-Top_html = open('./templates/top.html').read()
+top_html = open('./templates/top.html').read()
 bottom_html = open('./templates/bottom.html').read()
-middle_html = open('./content/index.html').Read(
-combined_html = Top_html + middle_html + bottom_html
+middle_html = open('./content/index.html').read()
+combined_html = top_html + middle_html + bottom_html
 open ('./docs/index.html', 'w+').write(combined_html)
-print (combined_html
+print (combined_html)
 middle_html = open('./content/about.html').read()
 combined_html = top_html + middle_html + bottom_html
 open ('./docs/about.html', 'w+').write(combined_html)
 print (combined_html)
-middle_html = Open('./content/contact.html').read(
+middle_html = open('./content/contact.html').read()
 combined_html = top_html + middle_html + bottom_html
-open ('./docs/contact.html', 'w+').write(combined_html)
+open ('./docs/contact.html','w+').write(combined_html)
 
-Print (combined_html)
+print (combined_html)
 middle_html = open('./content/blog.html').read()
 combined_html = top_html + middle_html + bottom_html
-open ('./docs/blog.html', 'w+').write(combined_html
+open ('./docs/blog.html', 'w+').write(combined_html)
 
 print (combined_html)
 middle_html = open('./content/testimonials.html').read()
 combined_html = top_html + middle_html + bottom_html
 open ('./docs/testimonials.html', 'w+').write(combined_html)
 
-print (combined_html
+print (combined_html)
 combined_html = top_html + middle_html + bottom_html
 #./doc/  move images and css files to docs
 #top and bottom do not need
---
+++ build.sh
@@ -0,0 +1,6 @@
+  
+cat templates/top.html content/index.html templates/bottom.html > docs/index.html
+cat templates/top.html content/about.html templates/bottom.html > docs/about.html
+cat templates/top.html content/blog.html templates/bottom.html > docs/blog.html
+cat templates/top.html content/contact.html templates/bottom.html > docs/contact.html
+cat templates/top.html content/testimonials.html templates/bottom.html > docs/testimonials.html
